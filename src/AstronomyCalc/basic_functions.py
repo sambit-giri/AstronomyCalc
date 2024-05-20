e552a07a49808088fd22918d5e4cb58d7763aa21
sambit-giri <sambit.giri@gmail.com>
2024-05-20 11:10:40 +0200

    docstring added

--- src/AstronomyCalc/basic_functions.py
+++ src/AstronomyCalc/basic_functions.py
@@ -1,7 +1,23 @@
 import numpy as np 
 
 def z_to_a(z):
+    '''
+    Convert redshift (z) to scale factor (a).
+    
+    Parameters:
+        param (float or numpy array): redshift value(s).
+    Returns:
+        scale factor
+    '''
     return 1/(1+z)
 
 def a_to_z(a):
+    '''
+    Convert scale factor (a) to redshift (z).
+    
+    Parameters:
+        param (float or numpy array): scale factor value(s).
+    Returns:
+        redshifts
+    '''
     return 1./a-1
