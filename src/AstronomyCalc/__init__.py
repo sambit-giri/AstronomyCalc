96068371b537e86783044f42e4e900df4f5ba80b
sambit-giri <sambit.giri@gmail.com>
2024-05-20 11:48:08 +0200

    SPARC data reader moved

--- src/AstronomyCalc/__init__.py
+++ src/AstronomyCalc/__init__.py
@@ -1,7 +1,7 @@
 
 from .param import *
 from .basic_functions import *
-from .io_functions import *
+# from .io_functions import *
 
 from .cosmo_calculator import *
 from .cosmo_equations import * 
--- src/AstronomyCalc/create_data.py
+++ src/AstronomyCalc/create_data.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 from astropy.cosmology import LambdaCDM
+from astropy import units as u 
+import os, requests, zipfile, pkg_resources, wget
 
 from .cosmo_equations import *
 
@@ -35,7 +37,7 @@ def distance_modulus(n_samples=100, z0=0.3, dmu_0=0.1, dmu_1=0.02, random_state=
 
 def Hubble1929_data(data_link=None):
     """
-    Load the 1929 Hubble dataset of distance vs. velocity.
+    Load the Hubble (1929) dataset of distance vs velocity.
 
     Parameters:
         data_link (str): URL to the CSV file containing the Hubble 1929 data. 
@@ -51,3 +53,74 @@ def Hubble1929_data(data_link=None):
     df = pd.read_csv(data_link)
     # print(df.head())
     return np.array(df['distance']), np.array(df['velocity'])
+
+class SPARC_Galaxy_dataset:
+    """
+    A class to handle the SPARC Galaxy dataset (http://astroweb.cwru.edu/SPARC/).
+
+    This class can download, read, and process files from the SPARC Galaxy dataset.
+
+    Attributes:
+        package_folder (str): Path to the package input data folder.
+        data_folder (str): Path to the folder containing the rotation curve data.
+    """
+
+    def __init__(self):
+        """
+        Initializes the SPARC_Galaxy_dataset class.
+
+        If the data folder does not exist, it triggers the download of the dataset.
+        """
+        self.package_folder = pkg_resources.resource_filename('AstronomyCalc', 'input_data/')
+        self.data_folder = os.path.join(self.package_folder, "Rotmod_LTG")
+        if not os.path.exists(self.data_folder):
+            self.download_data()
+
+    def read_rotation_curves(self, filename=None, name=None):
+        """
+        Reads the rotation curves from the SPARC dataset.
+
+        Parameters:
+            filename (str): The path to the rotation curve file.
+            name (str): The name of the galaxy. If provided, constructs the filename.
+
+        Returns:
+            dict: A dictionary containing the values and units of the rotation curve data.
+
+        Raises:
+            AssertionError: If neither filename nor name is provided.
+        """
+        assert filename is not None or name is not None, "Either filename or name must be provided."
+        
+        quantities = ['Rad', 'Vobs', 'errV', 'Vgas', 'Vdisk', 'Vbul', 'SBdisk', 'SBbul']
+        units_ = ['kpc', 'km/s', 'km/s', 'km/s', 'km/s', 'km/s', 'L/pc^2', 'L/pc^2']
+        
+        if name is not None:
+            filename = os.path.join(self.data_folder, f"{name}_rotmod.dat")
+        
+        rd = np.loadtxt(filename)
+        values = {quantities[i]: rd[:, i] for i in range(len(quantities))}
+        units = {quantities[i]: units_[i] for i in range(len(quantities))}
+        
+        return {'values': values, 'units': units}
+
+    def download_data(self):
+        """
+        Downloads and extracts the SPARC Galaxy dataset.
+
+        Downloads the dataset from the specified URL and extracts it to the target folder.
+        """
+        url = "http://astroweb.cwru.edu/SPARC/Rotmod_LTG.zip"
+        target_folder = self.package_folder
+        
+        wget.download(url, target_folder)
+        
+        zip_file_path = os.path.join(target_folder, "Rotmod_LTG.zip")
+        with zipfile.ZipFile(zip_file_path, "r") as zip_ref:
+            zip_file_target = os.path.join(target_folder, "Rotmod_LTG")
+            zip_ref.extractall(zip_file_target)
+        
+        os.remove(zip_file_path)
+        print("...download and extraction successful.")
+
+        return zip_file_target
