249003766b2eaa899f82d91d75b320bb177163db
sambit-giri <sambit.giri@gmail.com>
2024-08-20 10:20:04 +0200

    CI created

--- setup.py
+++ setup.py
@@ -7,17 +7,18 @@ Setup script
 from setuptools import setup, find_packages
 # from distutils.core import setup
 
+# Read requirements from requirements.txt
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
 
 setup(name='AstronomyCalc',
-      version='0.0.2',
+      version='0.1.0',
       author='Sambit Giri',
-      author_email='sambit.giri@su.se',
+      author_email='sambit.giri@gmail.com',
       packages=find_packages("src"),
       package_dir={"": "src"},
       package_data={'AstronomyCalc': ['input_data/*']},
-      install_requires=['numpy', 'scipy', 'matplotlib',
-                        'pytest', 'astropy', 'astroML',
-                        'pandas','emcee','corner','wget'],
+      install_requires=requirements,
       include_package_data=True,
       long_description=open('README.md').read(),
       long_description_content_type='text/markdown',
