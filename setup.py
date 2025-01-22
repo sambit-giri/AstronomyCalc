247b22364b2802ef47b6d682c783008875b918c0
sambit-giri <sambit.giri@gmail.com>
2025-01-22 16:27:56 +0100

    release for JOSE publication

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(name='AstronomyCalc',
-      version='1.0.2',
+      version='1.1.0',
       author='Sambit Giri',
       author_email='sambit.giri@gmail.com',
       packages=find_packages("src"),
